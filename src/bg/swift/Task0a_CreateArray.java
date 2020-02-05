b0a14b3d4467762c0fd689c6c922f870b8003520
Hristo <hristo_ixa@abv.bg>
2020-02-06 00:16:58 +0200

    Task0a_CreateArray

---
+++ src/bg/swift/Task0a_CreateArray.java
@@ -0,0 +1,11 @@
+package bg.swift;
+
+public class Task0a_CreateArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 11, 3, 6, 4, 7};
+
+        for (int a : arr) {
+            System.out.println(a);
+        }
+    }
+}
