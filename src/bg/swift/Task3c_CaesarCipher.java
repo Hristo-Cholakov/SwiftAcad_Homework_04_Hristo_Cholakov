c4db42f8de8bacbdfce797255b5f34118f3e45fe
cholakv <hristo_ixa@abv.bg>
2020-02-09 13:34:01 +0200

    Ceasar Cipher added

--- src/bg/swift/Task3c_CaesarCipher.java
+++ src/bg/swift/Task3c_CaesarCipher.java
@@ -1,4 +1,23 @@
 package bg.swift;
 
+import java.util.Scanner;
+
 public class Task3c_CaesarCipher {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+        String key = scanner.nextLine();
+        char[]splitedSentence = sentence.toCharArray();
+
+        for (char element :
+                splitedSentence) {
+            if (key.equals("encode")) {
+                element++;
+                System.out.printf("%s ", element);
+            } else {
+                element--;
+                System.out.printf("%s ", element);
+            }
+        }
+    }
 }
