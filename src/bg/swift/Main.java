7a413520c65f23d2b20ce0823c93ee3c7c1e7228
Hristo <hristo_ixa@abv.bg>
2020-02-10 00:44:17 +0200

    Spiral matrix attempt

--- src/bg/swift/Main.java
+++ src/bg/swift/Main.java
@@ -6,22 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine().toLowerCase();
-        int[] alphabetArray = new int[26];
-        for ( int i = 0; i < input.length(); i++ ) {
-            char ch=  input.charAt(i);
-            int value = (int) ch;
-            if (value >= 97 && value <= 122){
-                alphabetArray[ch-'a']++;
-            }
-        }
-        for (int i = 0; i < alphabetArray.length; i++) {
-            if(alphabetArray[i]>0){
-                char ch = (char) (i+97);
-                System.out.println(ch +"  : "+alphabetArray[i]);   //Show the result.
-            }
-        }
+
         }
 
     }
