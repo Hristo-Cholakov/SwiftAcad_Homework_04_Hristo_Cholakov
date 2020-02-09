0fc51d39c6a5286eb2234c7d94ac37727fc70477
Hristo <hristo_ixa@abv.bg>
2020-02-10 01:05:46 +0200

    Find K in sorted

--- src/bg/swift/Task2i_FindKInSorted.java
+++ src/bg/swift/Task2i_FindKInSorted.java
@@ -1,4 +1,23 @@
 package bg.swift;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task2i_FindKInSorted {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] sortedArray = new int[n];
+        for (int i = 0; i < sortedArray.length; i++) {
+            sortedArray[i] = scanner.nextInt();
+        }
+        int output = Arrays.binarySearch(sortedArray, k);
+        if (output >= 0) {
+            System.out.println(output);
+        }else {
+            System.out.println("NO");
+        }
+
+    }
 }
