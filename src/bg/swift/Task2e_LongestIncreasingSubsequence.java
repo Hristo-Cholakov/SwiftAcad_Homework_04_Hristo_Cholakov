9ffa1ab6140a835f087c893a4854a76d01742be5
Hristo <hristo_ixa@abv.bg>
2020-02-10 23:53:32 +0200

    LIS

--- src/bg/swift/Task2e_LongestIncreasingSubsequence.java
+++ src/bg/swift/Task2e_LongestIncreasingSubsequence.java
@@ -1,4 +1,20 @@
 package bg.swift;
 
+import java.util.Scanner;
+
 public class Task2e_LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                System.out.printf("%d ", arr[i-1]);
+            }
+            System.out.printf("%d", arr[i]);
+            }
+        }
 }
